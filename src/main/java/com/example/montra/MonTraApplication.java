417960860dd563f6a140a9dc91687cd4eddefa25
Sandip Sarraf <Sandip.Sarraf@Student.HTW-Berlin.de>
2025-02-11 21:15:24 +0100

    first commit

---
+++ src/main/java/com/example/montra/MonTraApplication.java
@@ -0,0 +1,13 @@
+package com.example.montra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonTraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonTraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/montra/MonTraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.montra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonTraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
