8d4cd3e10f4a5b3ebb408a31ca347ea3c4b944df
Sandip Sarraf <Sandip.Sarraf@Student.HTW-Berlin.de>
2025-02-12 10:24:35 +0100

    Completed the project

---
+++ src/main/java/com/example/montra/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.example.montra.repository;
+
+import com.example.montra.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
